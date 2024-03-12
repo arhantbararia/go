8320c06e8ed2eda08a329f29c7dae8fd3239c8ed
arhantbararia <arhantbararia@gmail.com>
2024-03-12 12:45:24 +0530

    strings and runes

---
+++ README.md
@@ -0,0 +1,6 @@
+# Go
+
+
+### UTF-8 encoding and loops with strings 
+
+https://chat.openai.com/share/71a06f51-0f5b-4949-bed8-0cd384492789
---
+++ faf/strings_runes_bytes.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+func main() {
+	var myString = "résumé"
+	fmt.Println(len(myString))
+	var indexed = myString[0]
+	fmt.Printf("%v, %T\n ", indexed, indexed)
+
+	for index, value := range myString {
+		fmt.Println(index, value)
+	}
+//output
+	// 	114, uint8
+	//  0 114
+	// 1 233
+	// 3 115
+	// 4 117
+	// 5 109
+	// 6 233
+	//since char é is 2 bytes long. it skips 2nd index. and when accessing these bytes it gets converted to uint8
+	// In loop1:
+
+    // - myString[0] corresponds to the first byte of the UTF-8 representation of the character 'résumé', which is 114 in decimal.
+    // - When iterating over the string using a range loop, it correctly splits the string into individual Unicode code points,
+	//  printing the index and the corresponding code point.
+
+	var indexed2 = myString[2]
+	fmt.Printf("%v, %T\n ", indexed2, indexed2)
+
+	intArr := [...]int32{0, 1, 2, 3, 4, 5, 6, 7}
+	for i, v := range intArr {
+		fmt.Println(i, byte(myString[v]))
+	}
+// output
+	// 0 114
+	// 1 195
+	// 2 169
+	// 3 115
+	// 4 117
+	// 5 109
+	// 6 195
+	// 7 169
+	// In loop2:
+
+    //- The intArr contains numbers 0 through 7.
+    //- The indices 1 and 6 correspond to the values 1 and 6 respectively in the intArr.
+    //- In Go, when you index a string with an integer, it interprets it as the index of the UTF-8 byte sequence of the string. 
+	// So, when accessing myString[v] with v being 1 and 6, it's attempting to access bytes at those indices. 
+	// These bytes represent parts of the UTF-8 encoding of the characters 'é' and 'é',
+	//  which result in different values than the Unicode code points.
+}
+
+
+
